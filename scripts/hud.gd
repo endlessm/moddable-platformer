8cbd85cccda43ccde253a2d0b1f5d96b66f771ff
Jian-Hong Pan <jhp@endlessos.org>
2024-06-04 15:17:47 +0800

    panel: Add colletecd coins information
    Add 1 more collected coin when the player enters a coin. Then, set the
    collected coins information on the panel.
    
    https://phabricator.endlessm.com/T35441

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -1,6 +1,18 @@
 extends CanvasLayer
 
 
+func _ready():
+	Global.coin_collected.connect(_on_coin_collected)
+
+
 func _unhandled_input(event):
 	if event is InputEventKey and %Start.is_visible_in_tree():
 		%Start.hide()
+
+
+func _on_coin_collected():
+	set_collected_coins(Global.coins)
+
+
+func set_collected_coins(coins: int):
+	%CollectedCoins.text = "Coins: " + str(coins)
