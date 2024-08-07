cf4e136ba16c03a7bf46a03e8fe6c234e4802f6e
Manuel Quiñones <manuel.por.aca@gmail.com>
2024-08-07 16:50:21 -0300

    HUD: Add label for global timer
    Above the collected coins label. Only if the timer is set, the label
    becomes visible. Also only enable processing the HUD node in that case
    for performance.
    
    https://phabricator.endlessm.com/T35597

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -6,9 +6,16 @@ extends CanvasLayer
 }
 
 
+func _process(_delta):
+	%TimeLeft.text = "%.1f" % Global.timer.time_left
+
+
 func _ready():
 	Global.coin_collected.connect(_on_coin_collected)
 	Global.game_ended.connect(_on_game_ended)
+	Global.timer_added.connect(_on_timer_added)
+	set_process(false)
+	set_physics_process(false)
 
 
 func _unhandled_input(event):
@@ -22,6 +29,11 @@ func _on_coin_collected():
 
 func set_collected_coins(coins: int):
 	%CollectedCoins.text = "Coins: " + str(coins)
+
+
+func _on_timer_added():
+	%TimeLeft.visible = true
+	set_process(true)
 
 
 func _on_game_ended(ending: Global.Endings):
